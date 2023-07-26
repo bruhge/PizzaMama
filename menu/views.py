723c447a291e50c74d1f84f09e456a7ad51b740d
bruhge <49010797+bruhge@users.noreply.github.com>
2023-07-26 12:57:34 -0500

    Add files via upload
    Added api ability

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from .models import Pizza
 
 
@@ -11,3 +12,9 @@ def index(request):
     return HttpResponse('Our Pizzas : ' + pizzas_names_str) '''
     pizzas = Pizza.objects.all().order_by('price')
     return render(request, 'menu/index.html', {'pizzas': pizzas})
+
+
+def api_get_pizza(request):
+    pizzas = Pizza.objects.all().order_by('price')
+    json = serializers.serialize('json', pizzas)
+    return HttpResponse(json)
